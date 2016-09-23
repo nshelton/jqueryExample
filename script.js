9e0b403b76d0cbcc82664435e602dc769bcf9dc9
nshelton <nshelton@gmail.com>
2016-09-22 20:59:25 -0700

    ANIMATIONS

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
+var images = {
+    "mer" : "sloth.jpg",
+    "pfffft" : "elephant.jpg",
+    "caw" : "seagull.jpg",
+    "rawr" : "lion.jpg",
+    "ribbit" : "frog.jpg"
+}
+
 $(function() {
     
+    for (key in images) {
+        if(images.hasOwnProperty(key)){
+            $("#"+key).prepend('<img src="img/' + images[key] +'"/>');
+        }
+    }
+
+
+
     $("#button1").click(function(){
         $("body").css("background-color","orange");
+         $(".animal img").animate({
+                left:'250px',
+                opacity:'0.5',
+                height: '100px',
+                width: '150px'
+            });
+
     });
     $("#button2").click(function(){
         var animals = $(".animal");
@@ -13,3 +36,4 @@ $(function() {
         }
     });
 });
+
